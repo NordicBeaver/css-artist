4fbb7fa7f5cd9424bb67c7b623c1f7f3537cd2eb
NordicBeaver <nordicbeaver@gmail.com>
2021-09-23 18:12:10 +0300

    Simple form for PaintingElement.

---
+++ src/conponents/PaintingElementForm.tsx
@@ -0,0 +1,82 @@
+import { FormControl, FormLabel } from '@chakra-ui/form-control';
+import { VStack } from '@chakra-ui/layout';
+import { SliderFilledTrack, SliderTrack } from '@chakra-ui/react';
+import { Slider, SliderThumb } from '@chakra-ui/slider';
+import React from 'react';
+import { PAINTING_HEIGHT, PAINTING_WIDTH } from '../constants';
+import { PaintingElement } from '../painting';
+
+export interface PaintingElementFormProps {
+  element: PaintingElement;
+  onChange?: (element: PaintingElement) => void;
+}
+export default function PaintingElementForm({ element, onChange }: PaintingElementFormProps) {
+  const handlePositionXSliderChange = (value: number) => {
+    onChange?.({
+      ...element,
+      posX: value,
+    });
+  };
+
+  const handlePositionYSliderChange = (value: number) => {
+    onChange?.({
+      ...element,
+      posY: value,
+    });
+  };
+
+  const handleWidthSliderChange = (value: number) => {
+    onChange?.({
+      ...element,
+      width: value,
+    });
+  };
+
+  const handleHeightSliderChange = (value: number) => {
+    onChange?.({
+      ...element,
+      height: value,
+    });
+  };
+
+  return (
+    <VStack>
+      <FormControl>
+        <FormLabel>Position X: {element.posX}</FormLabel>
+        <Slider min={0} max={PAINTING_WIDTH} value={element.posX} onChange={handlePositionXSliderChange}>
+          <SliderTrack>
+            <SliderFilledTrack></SliderFilledTrack>
+          </SliderTrack>
+          <SliderThumb></SliderThumb>
+        </Slider>
+      </FormControl>
+      <FormControl>
+        <FormLabel>Position Y: {element.posY}</FormLabel>
+        <Slider min={0} max={PAINTING_HEIGHT} value={element.posY} onChange={handlePositionYSliderChange}>
+          <SliderTrack>
+            <SliderFilledTrack></SliderFilledTrack>
+          </SliderTrack>
+          <SliderThumb></SliderThumb>
+        </Slider>
+      </FormControl>
+      <FormControl>
+        <FormLabel>Width: {element.width}</FormLabel>
+        <Slider min={0} max={PAINTING_WIDTH} value={element.width} onChange={handleWidthSliderChange}>
+          <SliderTrack>
+            <SliderFilledTrack></SliderFilledTrack>
+          </SliderTrack>
+          <SliderThumb></SliderThumb>
+        </Slider>
+      </FormControl>
+      <FormControl>
+        <FormLabel>Height: {element.height}</FormLabel>
+        <Slider min={0} max={PAINTING_HEIGHT} value={element.height} onChange={handleHeightSliderChange}>
+          <SliderTrack>
+            <SliderFilledTrack></SliderFilledTrack>
+          </SliderTrack>
+          <SliderThumb></SliderThumb>
+        </Slider>
+      </FormControl>
+    </VStack>
+  );
+}
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const PAINTING_WIDTH = 200;
+export const PAINTING_HEIGHT = 200;
